6070b874ededb835dae5ec85feea306b5406e13a
birkat-shahar <birkat.shahar@gmail.com>
2025-09-29 00:31:14 +0300

    Update articles.js

--- articles.js
+++ articles.js
@@ -2,21 +2,21 @@ const articles = [
   {
     title: "בדרך נעלמת",
     subject:"",
-    url: "https://drive.google.com/file/d/12T0DvpTfy39f-w5oiFdo3T-1h_habeo4/view?usp=sharing",
+    url: "https://drive.google.com/file/d/12T0DvpTfy39f-w5oiFdo3T-1h_habeo4/preview",
     category: "Hagim",
     subcategory: "HageyTishey"
   },
   {
     title: "וגם להשיב",
     subject:"",
-    url: "https://drive.google.com/file/d/1XCAuuA2FhiYTM8M_qMcVcC29WFWIx68Q/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/1XCAuuA2FhiYTM8M_qMcVcC29WFWIx68Q/preview",
     category: "Hagim",
     subcategory: "HageyTishey"
   },
   {
     title: "חג הסוכות",
     subject:"",
-    url: "https://drive.google.com/file/d/127ubjCpwgcQu2rFWULIOuC0YYDUp9xmz/view?usp=drive_link",
+    url: "https://drive.google.com/file/d/127ubjCpwgcQu2rFWULIOuC0YYDUp9xmz/preview",
     category: "Hagim",
     subcategory: "HageyTishey"
   }
