50acce3fb24a4f2b6ac0794427908c3a28ca0219
birkat-shahar <birkat.shahar@gmail.com>
2025-10-06 12:17:24 +0300

    Update topics.js

--- topics.js
+++ topics.js
@@ -4,6 +4,8 @@ const topics = [
     name: "חגים",
     subtopics: [
       { id: "HageyTishey", name: "חגי תשרי" },
+      { id: "RoshHashana", name: "ראש השנה" },
+      { id: "YomKipur", name: "יום כיפור" }     
     ]
   }
 ];
