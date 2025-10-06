44297f1fd988889e4c3403f39638fc037d2b5355
birkat-shahar <birkat.shahar@gmail.com>
2025-10-06 12:17:43 +0300

    Update articles.js

--- articles.js
+++ articles.js
@@ -19,6 +19,83 @@ const articles = [
     url: "https://drive.google.com/file/d/127ubjCpwgcQu2rFWULIOuC0YYDUp9xmz/preview",
     category: "Hagim",
     subcategory: "HageyTishey"
+  },
+  {
+    title: "יום כיפור - מערב עד ערב",
+    subject:"",
+    url: "https://drive.google.com/file/d/1lAqnXCdqexXg9RMqsLScgf7rnQ46ihu2/preview",
+    category: "Hagim",
+    subcategory: "YomKipur"
+  },
+  {
+    title: "אלול",
+    subject:"",
+    url: "https://drive.google.com/file/d/1cnT2CaUBZn6NTDcgScTp4si3T0IWVLTW/preview",
+    category: "Hagim",
+    subcategory: "HageyTishey"
+  },
+  {
+    title: "יום כיפור - אלומות אור",
+    subject:"",
+    url: "https://drive.google.com/file/d/1bkWN_pxJ_5KghPsvZOE1gU9AwLz4V2V1/preview",
+    category: "Hagim",
+    subcategory: "YomKipur"
+  },
+  {
+    title: "יום כיפור - משהו על ההסכם",
+    subject:"",
+    url: "https://drive.google.com/file/d/1zBLIfhFfM9bgiW9BS8iA04RxmSfgLvmk/preview",
+    category: "Hagim",
+    subcategory: "YomKipur"
+  },
+  {
+    title: "סליחות",
+    subject:"",
+    url: "https://drive.google.com/file/d/1oWLrjD4qW9tYkSvu2rSiYl1C9IRdQQ3E/preview",
+    category: "Hagim",
+    subcategory: "HageyTishey"
+  },
+  {
+    title: "ראש השנה - היום הרת עולם",
+    subject:"",
+    url: "https://drive.google.com/file/d/1kmXDK16am9PsH0DfvdFqBc6xtR3Ba7vL/preview",
+    category: "Hagim",
+    subcategory: "RoshHashana"
+  },
+  {
+    title: "ראש השנה - באשר הוא שם",
+    subject:"",
+    url: "https://drive.google.com/file/d/1r-vC_sBgrHgjFZd4GTGCbsP8XOZCtoJ5/preview",
+    category: "Hagim",
+    subcategory: "RoshHashana"
+  },
+  {
+    title: "ראש השנה המחזור",
+    subject:"",
+    url: "https://drive.google.com/file/d/1yjvO9yRql7xjGlBoVbnTM03SNDCygRSR/preview",
+    category: "Hagim",
+    subcategory: "RoshHashana"
+  },
+  {
+    title: "תשובה - איך ואיזה טעם",
+    subject:"",
+    url: "https://drive.google.com/file/d/1bNCpshIKsheZprgY2-DhkUXrjaEUySMv/preview",
+    category: "Hagim",
+    subcategory: "HageyTishey"
+  },
+  {
+    title: "ראש השנה - שנה טובה",
+    subject:"",
+    url: "https://drive.google.com/file/d/1TPTBnx4H5HMVcXf9AdRJnxL-2cSYyddO/preview",
+    category: "Hagim",
+    subcategory: "RoshHashana"
+  },
+  {
+    title: "תשובה - צעד אחד",
+    subject:"",
+    url: "https://drive.google.com/file/d/1bXDRzZDT00oETt_CWibNQjhviLv3bzvN/preview",
+    category: "Hagim",
+    subcategory: "HageyTishey"
   }
 ];
 
